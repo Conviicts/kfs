091fa026a8391fb6a72a17cbfc5710bc0b305b0d
jode-vri <jode-vri@student.42.fr>
2023-12-09 00:02:14 +0100

    gdt ok

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -13,9 +13,10 @@
 #include <kernel/gdt.h>
 
 struct gdt_entry gdt[5];
-struct gdt_ptr gdtp;
+struct gdt_ptr gdt_ptr;
 
 void create_descriptor(int pos, uint32_t base, uint32_t limit, uint16_t flag) {
+
     gdt[pos].limit_low = (limit & 0xFFFF);
 	gdt[pos].base_low = (base & 0xFFFF);
 	gdt[pos].base_middle = (base >> 16) & 0xFF;
@@ -26,13 +27,13 @@ void create_descriptor(int pos, uint32_t base, uint32_t limit, uint16_t flag) {
 }
 
 void init_gdt(void) {
-	gdtp.base = (uint32_t)&gdt;
-	gdtp.limit = (sizeof(struct gdt_ptr) * 5) - 1;
+	gdt_ptr.base = (uint32_t)&gdt;
+	gdt_ptr.limit = (sizeof(struct gdt_ptr) * 5) - 1;
 	create_descriptor(0, 0, 0, 0); // Null segment
 	create_descriptor(1, 0, 0x000FFFFF, (GDT_CODE_PL0)); // Kernel code
     create_descriptor(2, 0, 0x000FFFFF, (GDT_DATA_PL0)); // Kernel data
     create_descriptor(3, 0, 0x000FFFFF, (GDT_CODE_PL3)); // User code
     create_descriptor(4, 0, 0x000FFFFF, (GDT_DATA_PL3)); // User data
 
-	asm volatile("lgdt %0" : : "m" (gdtp)); // Load the GDT in GDTR register
+	asm volatile("lgdt %0" : : "m" (gdt_ptr)); // Load the GDT in GDTR register
 }
