65d9e1f7c3adf5e38c1a4e8b2c5cce475b1814b3
jode-vri <jode-vri@student.42.fr>
2023-12-18 10:04:39 +0100

    cursor move

--- kernel/arch/i386/tty/tty.c
+++ kernel/arch/i386/tty/tty.c
@@ -1,22 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   screen.c                                           :+:      :+:    :+:   */
+/*   tty.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jode-vri <jode-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 14:59:33 by jode-vri          #+#    #+#             */
-/*   Updated: 2023/12/06 15:18:37 by jode-vri         ###   ########.fr       */
+/*   Updated: 2023/12/18 10:01:02 by jode-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <kernel/tty.h>
+#include <kernel/io.h>
+
+static void tty_move_cursor(unsigned short pos) {
+	outb(0x3D4, 14);
+	outb(0x3D5, ((pos >> 8) & 0x00FF));
+	outb(0x3D4, 15);
+  	outb(0x3D5, pos & 0x00FF);
+}
 
 void	tty_putentryat(char c, uint8_t color, size_t x, size_t y) {
 	const size_t i = y * VGA_WIDTH + x;
 
 	tty.screen[i] = vga_entry(c, color);
 	tty.vga_buffer[i] = vga_entry(c, color);
+	tty_move_cursor(i + 1);
 }
 
 static void	tty_printchar(char c) {
