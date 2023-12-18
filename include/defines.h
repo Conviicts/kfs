ac1bb2b4475504ee7c5ade3668d7e84207bcf49f
jode-vri <jode-vri@student.42.fr>
2023-12-18 09:39:25 +0100

    Update typedefs in defines.h

--- include/defines.h
+++ include/defines.h
@@ -6,17 +6,16 @@
 /*   By: jode-vri <jode-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 03:03:48 by jode-vri          #+#    #+#             */
-/*   Updated: 2023/12/06 03:05:34 by jode-vri         ###   ########.fr       */
+/*   Updated: 2023/12/18 09:09:03 by jode-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _DEFINES_H
 #define _DEFINES_H
 
-#define size_t unsigned long
-#define uint8_t unsigned char
-#define uint16_t unsigned short
-#define uint32_t unsigned int
-#define uint64_t unsigned long
+typedef unsigned long    size_t;
+typedef unsigned char    uint8_t;
+typedef unsigned short   uint16_t;
+typedef unsigned int     uint32_t;
 
 #endif
