21a0450d36ec9c6d3a292726b694a7e5923f6b40
jode-vri <jode-vri@student.42.fr>
2023-12-19 10:35:39 +0100

    Refactor inb and outb functions to improve readability and add usage examples

--- kernel/arch/i386/drivers/asm/io.s
+++ kernel/arch/i386/drivers/asm/io.s
@@ -2,19 +2,41 @@ global inb
 global outb
 
 ; inb - returns a byte from the given I/O port
-; stack: [esp + 4] The address of the I/O port
-;        [esp    ] The return address
+; 
+; Input:    [esp + 4] - The address of the I/O port
+; Output:   AL - The byte read from the I/O port
+; Clobbers: DX - Used for storing the I/O port address
+; 
+; Description: Reads a byte from the specified I/O port and returns it.
+; 
+; Usage Example:
+;   push 0x3F8        ; I/O port address for COM1 serial port
+;   call inb          ; Call the inb function to read a byte
+;   ; The read byte is now in the AL register
+; 
 inb:
-	mov dx, [esp + 4]		; move the address of the I/O port to the dx register
-	in  al, dx				; read a byte from the I/O port and store it in the al register
-	ret						; return the read byte
+    mov dx, [esp + 4]   ; Move the address of the I/O port to the DX register
+    in  al, dx          ; Read a byte from the I/O port and store it in the AL register
+    ret                 ; Return the read byte
+
 
 ; outb - send a byte to an I/O port
-; stack: [esp + 8] the data byte
-;        [esp + 4] the I/O port
-;        [esp    ] return address
+; 
+; Input:    [esp + 8] - The data byte to be sent
+;           [esp + 4] - The I/O port address
+; Output:   None
+; Clobbers: AL - Used for storing the data byte, DX - Used for storing the I/O port address
+; 
+; Description: Sends a byte to the specified I/O port.
+; 
+; Usage Example:
+;   push 0x41         ; Data byte to be sent
+;   push 0x3F8        ; I/O port address for COM1 serial port
+;   call outb         ; Call the outb function to send the byte
+;   ; The byte has been sent to the specified I/O port
+; 
 outb:
-	mov al, [esp + 8]	; move the data to be sent into the al register
-	mov dx, [esp + 4]	; move the address of the I/O port into the dx register
-	out dx, al			; send the data to the I/O port
-	ret					; return to the calling function
+    mov al, [esp + 8]   ; Move the data to be sent into the AL register
+    mov dx, [esp + 4]   ; Move the address of the I/O port into the DX register
+    out dx, al          ; Send the data to the I/O port
+    ret                 ; Return to the calling function
